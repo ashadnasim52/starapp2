4ea6cda54f55c18b1148f9a30ea095162ff541f5
root <ashadnasim123@gmail.com>
2018-05-10 08:05:04 +0530

    test

---
+++ app/src/main/java/com/superoriginal/rootashadnasim/starapp/fgdfg.java
@@ -0,0 +1,13 @@
+package com.superoriginal.rootashadnasim.starapp;
+
+class fgdfg {
+    private static final fgdfg ourInstance = new fgdfg();
+
+    static fgdfg getInstance() {
+        return ourInstance;
+    }
+
+    private fgdfg() {
+        //fsdfdfdfdf
+    }
+}
